62fb2f4539bd756bbd5b162949fa00af55bc37a6
nomicos <nomicos@users.noreply.github.com>
2016-07-01 11:16:21 +0300

    Add screen freezing after winning the game

--- main.cpp
+++ main.cpp
@@ -16,5 +16,9 @@ int main()
     // Entering menu for that game.
     Menu::ask(g);
 
+    // Freeze the screen; dunno why one cin.get() doesn't work.
+    std::cin.get();
+    std::cin.get();
+
     return 0;
 }
