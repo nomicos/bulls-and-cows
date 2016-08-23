3e4490ad1864cf89dc5bf6eeaffc0ceb442676c4
Andrii Bida <nomicos@users.noreply.github.com>
2016-08-23 09:12:31 +0300

    Clarify the exit hint

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ int main()
     g.menu();
 
     // Freezing the screen.
-    std::cout << "Press any key to close the window.\n";
+    std::cout << "Press any key to exit.\n";
     std::cin.get();
 
     return 0;
